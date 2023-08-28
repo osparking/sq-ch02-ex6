ae1b1a010dfd2ce82c8a6cf3ab7a0fa38e2388d0
osparking <jbpark03@gmail.com>
2023-08-28 21:13:29 +0900

    specified base package of component

--- src/main/java/ssh/ch02_ex6/config/ProjectConfig.java
+++ src/main/java/ssh/ch02_ex6/config/ProjectConfig.java
@@ -1,8 +1,11 @@
 package ssh.ch02_ex6.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "ssh.ch02_ex6.entity")
+
 public class ProjectConfig {
 
 }
