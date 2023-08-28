eb372aa30cbf0178a17f589025efa2481f7ca55e
osparking <jbpark03@gmail.com>
2023-08-28 17:46:27 +0900

    2 classes : config, parrot

---
+++ src/main/java/ssh/ch02_ex6/config/ProjectConfig.java
@@ -0,0 +1,8 @@
+package ssh.ch02_ex6.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+}
---
+++ src/main/java/ssh/ch02_ex6/entity/Parrot.java
@@ -0,0 +1,8 @@
+package ssh.ch02_ex6.entity;
+
+import lombok.Data;
+
+@Data
+public class Parrot {
+	private String name;
+}
